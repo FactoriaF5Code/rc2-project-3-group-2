19df5e34575e97ab350bab4b6e404fdd0f4ad1b7
San-QP <s.rebollar.g@gmail.com>
2023-12-07 10:54:33 +0100

    Iconos de reproducción añadidos y pruebas display en Javascript

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ let soundTriste = document.getElementById("soundTriste");
 let soundEnfadado = document.getElementById("soundEnfadado");
 let soundMiedoso = document.getElementById("soundMiedoso");
 
+let showReproduccionFeliz = document.getElementById("iconMusicOnFeliz");
+let showReproduccionTriste = document.getElementById("iconMusicOnTriste");
+let showReproduccionEnfadado = document.getElementById("iconMusicOnEnfadado");
+let showReproduccionMiedoso = document.getElementById("iconMusicOnMiedoso");
+
 function stopAllSounds() {
     soundFeliz.pause();
     soundFeliz.currentTime = 0;
@@ -17,14 +22,20 @@ function stopAllSounds() {
     soundEnfadado.currentTime = 0;
     soundMiedoso.pause();
     soundMiedoso.currentTime = 0;
+    showReproduccionFeliz.style.display = "none";
+    showReproduccionTriste.style.display = "none";
+    showReproduccionEnfadado.style.display = "none";
+    showReproduccionMiedoso = "none";
 }
 
 emojiOnFeliz.onclick = function() {
     if (soundFeliz.paused) {
         stopAllSounds();
         soundFeliz.play();
+        showReproduccionFeliz.style.display = "block";
     } else {
         soundFeliz.pause();
+        showReproduccionFeliz.style.display = "none";
     }
 }
 
@@ -32,8 +43,10 @@ emojiOnTriste.onclick = function() {
     if (soundTriste.paused) {
         stopAllSounds();
         soundTriste.play();
+        showReproduccionTriste.style.display = "block";
     } else {
         soundTriste.pause();
+        showReproduccionTriste.style.display = "none";
     }
 }
 
@@ -41,8 +54,10 @@ emojiOnEnfadado.onclick = function() {
     if (soundEnfadado.paused) {
         stopAllSounds();
         soundEnfadado.play();
+        showReproduccionEnfadado.style.display = "block";
     } else {
         soundEnfadado.pause();
+        showReproduccionEnfadado.style.display = "none";
     }
 }
 
@@ -50,7 +65,10 @@ emojiOnMiedoso.onclick = function(){
     if (soundMiedoso.paused) {
         stopAllSounds();
         soundMiedoso.play();
+        showReproduccionMiedoso = "block";
     } else {
         soundMiedoso.pause();
+        showReproduccionMiedoso = "none";
     }
 }
+
