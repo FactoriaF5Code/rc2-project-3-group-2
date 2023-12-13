5d04cbaa7c0556efdaf21c2338f13ab10d41e7a9
San-QP <s.rebollar.g@gmail.com>
2023-12-13 14:14:35 +0100

    Historia de usuario 3: Contenido alegría añadido en html y funciones para ocultar/mostrar en js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ let emojiOnEnfadado = document.getElementById("enfadado");
 let emojiOnMiedoso = document.getElementById("miedoso");
 let iconsStop = document.getElementsByClassName("iconStop");
 
-
 let soundFeliz = document.getElementById("soundFeliz");
 let soundTriste = document.getElementById("soundTriste");
 let soundEnfadado = document.getElementById("soundEnfadado");
@@ -216,3 +215,59 @@ document.onkeydown = function(e) {
     }
 }
 
+
+
+//Función para mostrar el contenido de Alegría
+const botonAlegria = document.getElementById("botonAlegria");
+const contentAlegria = document.getElementById("contentAlegria");
+
+// Agregamos un evento de clic al botón
+botonAlegria.addEventListener("click", function() {
+  // Verificamos si el contenido ya está visible o no
+  if (contentAlegria.style.display === "none" || contentAlegria.style.display === "") {
+    // Si no está visible, lo mostramos
+    contentAlegria.style.display = "flex";
+    contentAlegria.style.justifyContent = "center";
+    contentAlegria.style.alignItems = "center";
+    contentAlegria.style.flexDirection = "column"
+  } else {
+    // Si ya está visible, lo ocultamos
+    contentAlegria.style.display = "none";
+  }
+});
+
+
+//Función para mostrar/ocultar las flechas
+let flechaYellowActive = document.getElementById("flechaYellow");
+let flechaGreenActive = document.getElementById("flechaGreen");
+let flechaRedActive = document.getElementById("flechaRed");
+let flechaPurpleActive = document.getElementById("flechaPurple");
+
+botonAlegria.addEventListener("click", function() {
+    // Verificamos si la opacidad es 0 o 100
+    if (flechaYellowActive.style.opacity === "0" || flechaYellowActive.style.opacity === "") {
+      // Si es 0, lo subimos a 100
+      flechaYellowActive.style.opacity = "100";
+    } else {
+      // Si es 100, lo bajamos a 0
+      flechaYellowActive.style.opacity = "0";
+    }
+  });
+
+
+
+// Función para reproducir la melodía alegría en midi
+const listenMelodyElement = document.querySelector(".listenMelody");
+const melodiaAlegriaAudio = document.getElementById("melodiaAlegria");
+
+// Agregamos un evento de clic al elemento "listenMelody"
+listenMelodyElement.addEventListener("click", function() {
+  // Verificamos si el audio ya está reproduciéndose o no
+  if (melodiaAlegriaAudio.paused) {
+    // Si está en pausa, lo reproducimos
+    melodiaAlegriaAudio.play();
+  } else {
+    // Si está reproduciéndose, lo pausamos
+    melodiaAlegriaAudio.pause();
+  }
+});
